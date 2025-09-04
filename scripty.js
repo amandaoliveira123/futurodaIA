9ddb689acefe0e3483453a53f2930ce8780fd90f
amandaoliveira123 <145065409+amandaoliveira123@users.noreply.github.com>
2025-09-04 17:05:56 -0300

    ..

--- scripty.js
+++ scripty.js
@@ -6,7 +6,38 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
 {
-        enunciado: "Pergunta 1",
+        enunciado: "A IA pode ajudar na medicina?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "Sim, pode auxiliar no diagnóstico de doenças.",
+            "Não, nunca poderá ser usada em hospitais."
+        ]
+},
+{
+        enunciado: "A IA vai substituir todos os empregos?",
+        alternativas: [
+            "Não, vai transformar e criar novas profissões.",
+            "Sim, ninguém mais precisará trabalhar."
+        ]
+},
+{
+        enunciado: "A IA pode aprender com os erros?",
+        alternativas: [
+            "Sim, ela pode melhorar com novos dados.",
+            "Não, sempre repete os mesmos erros.
+"
+        ]
+},
+{
+        enunciado: "A IA pode ser usada na educação?",
+        alternativas: [
+            "Sim, pode ajudar alunos a aprender mais rápido.",
+            "Não, porque não entende nada de estudo."
+        ]
+},
+{
+        enunciado: "A IA pode ser perigosa se mal usada?",
+        alternativas: [
+            "Sim, precisa de regras para evitar riscos.",
+            "Não, é sempre totalmente segura."
+        ]
+},
